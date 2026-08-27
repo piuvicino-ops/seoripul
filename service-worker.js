23e35840735095c71746e4120e0b24c0d769d5b3
piuvicino-ops <piuvicino@gmail.com>
2026-08-27 23:48:40 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,33 +1,37 @@
-const CACHE_NAME = 'seoripul1-v1';
+const CACHE_NAME = 'seoripul1-v2';
 const APP_SHELL = [
   './',
   './index.html',
   './manifest.webmanifest',
-  './icon-512-1.png'
+  './icon-512-1.png',
+  './18005.jpg',
+  './18022.jpg',
+  './18035.jpg',
+  './18037.jpg'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL))
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(APP_SHELL))
+      .then(() => self.skipWaiting())
   );
-  self.skipWaiting();
 });
 
 self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(keys =>
       Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))
-    )
+    ).then(() => self.clients.claim())
   );
-  self.clients.claim();
 });
 
 self.addEventListener('fetch', event => {
   if (event.request.method !== 'GET') return;
-
   event.respondWith(
     caches.match(event.request).then(cached => {
-      return cached || fetch(event.request).then(response => {
+      if (cached) return cached;
+      return fetch(event.request).then(response => {
         const copy = response.clone();
         caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
         return response;
